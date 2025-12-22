b5f544b758626a7c2265252c621cc43594e7161e
DaviSiqueira <74602032+davisiqueirat@users.noreply.github.com>
2025-12-21 21:57:14 -0300

    Update Header.tsx

--- FrontEnd/src/Components/Header.tsx
+++ FrontEnd/src/Components/Header.tsx
@@ -1,9 +1,8 @@
 import { Link } from 'react-router-dom';
 
 export function Header() {
-    // Estilos Inline para facilitar (pode levar pro CSS depois se quiser)
     const headerStyle = {
-        backgroundColor: '#2c3e50', // Azul Petróleo Escuro (Profissional)
+        backgroundColor: '#2c3e50', 
         padding: '15px 0',
         marginBottom: '30px',
         boxShadow: '0 2px 5px rgba(0,0,0,0.1)'
@@ -29,7 +28,7 @@ export function Header() {
     };
 
     const navLinkStyle = {
-        color: '#ecf0f1', // Cinza bem claro
+        color: '#ecf0f1',
         textDecoration: 'none',
         fontWeight: 500,
         fontSize: '1rem',
@@ -40,12 +39,10 @@ export function Header() {
         <header style={headerStyle}>
             <div style={containerStyle}>
                 
-                {/* 1. O NOME CLICÁVEL (Vai para a Home) */}
                 <Link to="/" style={logoStyle}>
                     FinançasApp
                 </Link>
 
-                {/* 2. MENU DE NAVEGAÇÃO (Bônus: Links rápidos) */}
                 <nav style={{ display: 'flex', gap: '20px' }}>
                     <Link to="/" style={navLinkStyle}>Dashboard</Link>
                     <Link to="/pessoas" style={navLinkStyle}>Pessoas</Link>
