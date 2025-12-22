13b39659091fc457a0fac40967095014eca41e2a
DaviSiqueira <74602032+davisiqueirat@users.noreply.github.com>
2025-12-21 22:01:13 -0300

    Update api.ts

--- FrontEnd/src/Services/api.ts
+++ FrontEnd/src/Services/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // Atenção: Confirme se a porta do seu Swagger é 7033 ou outra.
-    // Geralmente é https://localhost:7033 ou http://localhost:5xxx
-    baseURL: 'https://localhost:7033/api', 
+    baseURL: 'http://localhost:5279/api', 
     headers: {
         'Content-Type': 'application/json'
     }
